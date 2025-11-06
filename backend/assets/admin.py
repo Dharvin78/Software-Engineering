3e06291e12128eb3ae7a85346b731b08640a048d
lim0434 <chienhuilim0434@gmail.com>
2025-11-06 08:58:18 +0800

    Delete backend directory

--- backend/assets/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Asset
-
-@admin.register(Asset)
-class AssetAdmin(admin.ModelAdmin):
-    list_display = ('title', 'file_type', 'tags', 'uploaded_at')
-    list_filter = ('file_type', 'uploaded_at')
-    search_fields = ('title', 'description', 'tags')
-    ordering = ('-uploaded_at',)
