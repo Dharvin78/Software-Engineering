9278f3907d6a55e0baac56103a9ab037bb329b17
Dharvin78 <rsdavin2@gmail.com>
2025-11-07 20:20:15 +0800

    Metadata Management File

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+// src/pages/_app.tsx
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import theme from "../theme";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
